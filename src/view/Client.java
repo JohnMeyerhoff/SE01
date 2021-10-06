62c3f1f0a1de0df37291646ac08e91d086662600
John Meyerhoff <John@r73.de>
2021-10-06 11:53:32 +0200

    factory pattern fertig
    der generator gibt eine spezialform zurueck, wir behandeln die rueckgabe aber als Translator (interface)

--- src/view/Client.java
+++ src/view/Client.java
@@ -1,14 +1,16 @@
 package view;
 
 import controller.GermanTranslatorHelper;
+import controller.Translator;
 
 public class Client {
 	/*
 	 * Methode zur Ausgabe einer Zahl auf der Console
 	 */
 	public void display(int nummer) {
+		Translator generalTranslator = GermanTranslatorHelper.gtFactory();
 		System.out.println("Ergebnis: " + 
-			GermanTranslatorHelper.gtFactory().
+			generalTranslator.
 			translateNumber(nummer));
 	}
 }
