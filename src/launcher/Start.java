4f1520b7ea173ccbe6e85aae97d22404396bb62d
John <john@meyerhoff.org>
2021-10-12 21:45:49 +0200

    Update Start.java

--- src/launcher/Start.java
+++ src/launcher/Start.java
@@ -6,8 +6,10 @@ public class Start {
     public static void main(String[] args) {
         Client c = new Client();
         c.display(5);
-        c.display(7);
-        c.display(10);
-        c.display(0);
+        c.display(-5);
+        c.display(50);
+        c.display(55);
+        c.display(-75);
+        c.display(35);
     }
 }
