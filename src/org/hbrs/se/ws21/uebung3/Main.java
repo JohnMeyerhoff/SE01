87d84bf8af85aeeb75286222fdfc7eeac398c417
klara <firstemail@icloud.com>
2021-10-23 15:25:08 +0200

    main?

--- src/org/hbrs/se/ws21/uebung3/Main.java
+++ src/org/hbrs/se/ws21/uebung3/Main.java
@@ -1,12 +1,22 @@
 package org.hbrs.se.ws21.uebung3;
 
-import org.hbrs.se.ws21.uebung2.Container;
-
+import org.hbrs.se.ws21.uebung3.Container;
 public class Main {
-    Container objekt = new Container();
-    Member a;
-    Member b;
-    Member c;
-    Member d;
-    Member e;
+
+    public Container buildInstances(){
+        Container objekt = null;
+        Member a;
+        Member b;
+        Member c;
+        Member d;
+        Member e;
+        objekt.addMember(a);
+        objekt.addMember(a);
+        objekt.addMember(c);
+        objekt.addMember(d);
+        objekt.addMember(e);
+        return objekt;
+
+    }
+    
 }
