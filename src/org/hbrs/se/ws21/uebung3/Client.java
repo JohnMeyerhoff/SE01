d9b6625e50d12b84cfaf468449408a60cf92eb7f
klara <firstemail@icloud.com>
2021-10-23 15:23:52 +0200

    voila

---
+++ src/org/hbrs/se/ws21/uebung3/Client.java
@@ -0,0 +1,6 @@
+package org.hbrs.se.ws21.uebung3;
+
+public class Client {
+    
+    
+}
---
+++ src/org/hbrs/se/ws21/uebung3/Main.java
@@ -0,0 +1,12 @@
+package org.hbrs.se.ws21.uebung3;
+
+import org.hbrs.se.ws21.uebung2.Container;
+
+public class Main {
+    Container objekt = new Container();
+    Member a;
+    Member b;
+    Member c;
+    Member d;
+    Member e;
+}
