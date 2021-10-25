a8a6fa40612cba92b0230fc0fd958ed03208656c
John <john@meyerhoff.org>
2021-10-25 20:29:32 +0200

    refactor

--- src/org/hbrs/se/ws21/uebung3/persistence/PersistenceStrategyStream.java
+++ src/org/hbrs/se/ws21/uebung3/persistence/PersistenceStrategyStream.java
@@ -21,9 +21,9 @@ public class PersistenceStrategyStream implements PersistenceStrategy<Member> {
     private String location = "objects.ser";
     // private FileInputStream fileInput;
     // private FileOutputStream fileOutput;
-    private ObjectInputStream objectInput;
+    private ObjectInputStream  objectInput;
     private ObjectOutputStream objectOutput;
-    boolean connected = false;
+    boolean                    connected = false;
     // Backdoor method used only for testing purposes, if the location should be
     // changed in a Unit-Test
     // Example: Location is a directory (Streams do not like directories, so try
@@ -39,22 +39,22 @@ public class PersistenceStrategyStream implements PersistenceStrategy<Member> {
 
     @Override
     /**
-     * Method for opening the connection to a stream (here: Input- and
-     * Output-Stream) In case of having problems while opening the streams, leave
-     * the code in methods load and save
+     * Method for opening the connection to a stream (here: Input- and Output-Stream) In case of
+     * having problems while opening the streams, leave the code in methods load and save
      */
     public void openConnection() throws PersistenceException {
-        //CONNECTED besagt ob eine verbindung besteht
-        if (!connected) {  //es besteht noch keine Verbindung
-        try {
-            objectOutput = new ObjectOutputStream(new FileOutputStream(location));
-            objectInput = new ObjectInputStream(new FileInputStream(location));
-        } catch (IOException r) {
-            throw new PersistenceException(ExceptionType.ConnectionNotAvailable, r.getMessage());
-        }
-        connected = true;
-        }else{
-          //nicht in der Aufgabenstellung vorgegeben  
+        // CONNECTED besagt ob eine verbindung besteht
+        if (!connected) { // es besteht noch keine Verbindung
+            try {
+                objectOutput = new ObjectOutputStream(new FileOutputStream(location));
+                objectInput = new ObjectInputStream(new FileInputStream(location));
+            } catch (IOException r) {
+                throw new PersistenceException(ExceptionType.ConnectionNotAvailable,
+                        r.getMessage());
+            }
+            connected = true;
+        } else {
+            // nicht in der Aufgabenstellung vorgegeben
         }
     }
 
@@ -63,15 +63,16 @@ public class PersistenceStrategyStream implements PersistenceStrategy<Member> {
      * Method for closing the connections to a stream
      */
     public void closeConnection() throws PersistenceException {
-        if (connected) { //es gibt eine Verbindung
+        if (connected) { // es gibt eine Verbindung
             try {
                 objectInput.close();
                 objectOutput.close();
             } catch (IOException e) {
-                throw new PersistenceException(ExceptionType.ConnectionNotAvailable, e.getMessage());
-            } 
-        }else{ //es gibt keine zu schließende BEdingung
-            //nicht in der Augabnestellung behandelt
+                throw new PersistenceException(ExceptionType.ConnectionNotAvailable,
+                        e.getMessage());
+            }
+        } else { // es gibt keine zu schließende Verbindung
+                 // nicht in der Augabenstellung erwähnt
         }
     }
 
@@ -80,9 +81,9 @@ public class PersistenceStrategyStream implements PersistenceStrategy<Member> {
      * Method for saving a list of Member-objects to a disk (HDD)
      */
     public void save(List<Member> containerInhalt) throws PersistenceException {
-        if(!connected){
+        if (!connected) {
             openConnection();
-        } //es besteht auf jeden fall eine Verbindung
+        } // es besteht auf jeden fall eine Verbindung
         try {
             objectOutput.writeObject(containerInhalt);
             objectOutput.flush();
@@ -93,40 +94,23 @@ public class PersistenceStrategyStream implements PersistenceStrategy<Member> {
 
     @Override
     /**
-     * Method for loading a list of Member-objects from a disk (HDD) Some coding
-     * examples come for free :-) Take also a look at the import statements above
-     * ;-!
+     * Method for loading a list of Member-objects from a disk (HDD) Some coding examples come for
+     * free :-) Take also a look at the import statements above ;-!
      */
+    @SuppressWarnings("unchecked")
     public List<Member> load() throws PersistenceException {
-        // Some Coding hints ;-)
-        // ObjectInputStream ois = null;
-        // FileInputStream fis = null;
-        // List<...> newListe = null;
-        //
-        // Initiating the Stream (can also be moved to method openConnection()... ;-)
-        // fis = new FileInputStream( " a location to a file" );
-        // ois = new ObjectInputStream(fis);
-
-        // Reading and extracting the list (try .. catch ommitted here)
-        // Object obj = ois.readObject();
-
-        // if (obj instanceof List<?>) {
-        // newListe = (List) obj;
-        // return newListe
-        List<Member> newListe = null;
         if (!connected) {
             openConnection();
         } // es besteht auf jeden fall eine Verbindung
         try {
             Object tmp = objectInput.readObject();
-            if(tmp instanceof List<?>){
-                newListe =(List<Member>) tmp;
-                return newListe;
-            }else{
-                throw new PersistenceException(ExceptionType.ConnectionNotAvailable, "Saved data was not the correct type.");
-
+            if (tmp instanceof List<?>) {
+                return (List<Member>) tmp;
+            } else {
+                throw new PersistenceException(ExceptionType.ConnectionNotAvailable,
+                        "Saved data was not the correct type.");
             }
-        } catch (IOException| ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             throw new PersistenceException(ExceptionType.ConnectionNotAvailable, e.getMessage());
         }
         // and finally close the streams (guess where this could be...?)
