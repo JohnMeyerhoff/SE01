e03af8869913408356f95167af851560a337ebb8
John <john@meyerhoff.org>
2021-10-25 20:11:14 +0200

    update favorites

--- src/org/hbrs/se/ws21/uebung3/MemberView.java
+++ src/org/hbrs/se/ws21/uebung3/MemberView.java
@@ -9,13 +9,19 @@ public class MemberView {
         /**
          * In the current implementation, this method returns a copy of the current list.
          */
+        return Container.getInstance().inhalt;
+    }
+
+    public List<Member> getCurrentListCopy(){
+        /**
+         * In the current implementation, this method returns a copy of the current list.
+         */
         List<Member> result = new ArrayList<>();
         for (Member member : Container.getInstance().inhalt) {
             result.add(member);
         }
         return result;
     }
-
     public void dump(List<Member> liste){
         for (Member x : liste) {
             System.out.println(x.toString());
