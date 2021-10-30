eaaf97d3761cace2fee44a951790378007b0e503
klara <firstemail@icloud.com>
2021-10-30 21:57:16 +0200

    bittö
    bittö

--- src/org/hbrs/se/ws21/uebung3/MemberView.java
+++ src/org/hbrs/se/ws21/uebung3/MemberView.java
@@ -1,7 +1,6 @@
 package org.hbrs.se.ws21.uebung3;
 //Dieses Aufgabenblatt ist in Teamarbeit von Klara Golubovic 
 //und Johannes Meyerhoff bearbeitet worden.
-import java.util.ArrayList;
 import java.util.List;
 
 public class MemberView {
@@ -9,6 +8,8 @@ public class MemberView {
     public void dump(List<Member> liste){
         for (Member x : liste) {
             System.out.println(x.toString());
+            //alda: 
+            System.out.println("ID: " + x.getID());
         }
     } 
 }
