3369cb3cbadc228170b4f7f851c6074074736ddd
klara <firstemail@icloud.com>
2021-11-01 18:42:06 +0100

    hm

--- src/org/hbrs/se/ws21/uebung4/Client.java
+++ src/org/hbrs/se/ws21/uebung4/Client.java
@@ -1,23 +1,58 @@
 package org.hbrs.se.ws21.uebung4;
 //Dieses Aufgabenblatt ist in Teamarbeit von Klara Golubovic 
 //und Johannes Meyerhoff bearbeitet worden.
+import java.util.Scanner;
+
+import org.hbrs.se.ws21.uebung4.persistence.PersistenceException;
+
 public class Client {
-    public void displayContainerWithMemberview(Container storage, MemberView view){
-        Member a = new Mitarbeiter();
-        Member b = new Mitarbeiter();
-        Member c = new Mitarbeiter();
-        Member d = new Mitarbeiter();
-        Member e = new Mitarbeiter();
-        try {
-            storage.addMember(a);
-            storage.addMember(b);
-            storage.addMember(c);
-            storage.addMember(d);
-            storage.addMember(e);
-        } catch (Exception error) {
-            error.printStackTrace();
-        }
-        view.dump(storage.getCurrentList());
-    }
+    Scanner suche = new Scanner(System.in);
     
+    public int konsole() throws PersistenceException{
+        String tmp = suche.next();
+        while(suche.hasNext()){
+        if(tmp.equals("help")){
+            System.out.println(
+            "Mögliche Befehle für Sie sind: "+ "\n" + 
+            "enter,"+ "\n" +  
+            "store,"+ "\n" + 
+            "load,"+ "\n" + 
+            "dump,"+ "\n" + 
+            "search,"+ "\n" + 
+            "exit,"+ "\n" + 
+            "help"+ "\n");
+        }
+        if(tmp.equals("exit")){
+            System.out.println("Auf Wiedersehen");
+            return 0;
+        }
+        if(tmp.equals("store")){
+            Container.getInstance().store();
+        }
+        if (tmp.equals("load")) {
+            try{
+            if(suche.next().equals("merge")){
+
+            }else{
+
+            }
+            }catch(IllegalArgumentException e){
+                throw new IllegalArgumentException("Sie haben einen falschen Parameter angegeben.");
+            }
+        }
+
+        if (tmp.equals("dump")) {
+            MemberView a = new MemberView();
+             a.dumpSorted(Container.getInstance().getCurrentListCopy()))
+        }
+        
+        System.out.println("Sie haben keine der gegebenen Befehle benutzt. "+ "\n"+ 
+        "Geben Sie 'help' als Befehl ein, um alle Möglichkeiten zu sehen.");
+        
+        
+        
+        
+        }
+        return 5; //Eingabe beendet ohne exit
+    }
 }
--- src/org/hbrs/se/ws21/uebung4/MemberView.java
+++ src/org/hbrs/se/ws21/uebung4/MemberView.java
@@ -1,15 +1,37 @@
 package org.hbrs.se.ws21.uebung4;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 //Dieses Aufgabenblatt ist in Teamarbeit von Klara Golubovic 
 //und Johannes Meyerhoff bearbeitet worden.
 import java.util.List;
 
 public class MemberView {
-    
-    public void dump(List<Member> liste){
+
+    public void dump(List<Member> liste) {
         for (Member x : liste) {
             System.out.println(x.toString());
-            //alda: 
+            // alda:
             System.out.println("ID: " + x.getID());
         }
-    } 
+    }
+    private class MemberComparator implements Comparator<Member>{
+        public int compare(Member s1, Member s2) {
+            return s1.getID().compareTo(s2.getID());           
+            // s1 = 34 s2 = 2
+            // 1,2,3,4
+            }
+    }
+    
+
+    public void dumpSorted(List<Member> liste){
+        int i = 0;
+        Collections.sort(liste, new MemberComparator());
+        for (Member x : liste) {
+            System.out.println(x.toString());
+            // alda:
+            System.out.println("ID: " + x.getID());
+        }
+    }
 }
