a6b0153192255457a01ba536e1f0bea2ecac7b55
John Meyerhoff <John@r73.de>
2021-11-09 16:09:16 +0100

    updates

--- src/org/hbrs/se/ws21/uebung4/view/TabellenAusgabe.java
+++ src/org/hbrs/se/ws21/uebung4/view/TabellenAusgabe.java
@@ -6,25 +6,16 @@ import org.hbrs.se.ws21.uebung4.model.Expertise;
 import org.hbrs.se.ws21.uebung4.model.Mitarbeiter;
 
 public class TabellenAusgabe {
-
+    private static final String TABS = "\t\t\t\t";
     public void tabelle(MitarbeiterContainer a) {
-        System.out.println(
-                "Vorname\t\t\t\tName\t\t\t\tAbteilung\t\t\t\tRolle\t\t\t\tExpertisen");
+        System.out.println("Vorname\t\t\t\tName\t\t\t\tAbteilung\t\t\t\tRolle\t\t\t\tExpertisen");
         for (Mitarbeiter m : a.getCurrentList()) {
-            System.out.print(m.getVorname() + "\t\t\t\t");
-            System.out.print(m.getName() + "\t\t\t\t");
-            System.out.print(m.getAbteilung() + "\t\t\t\t");
-            System.out.print(m.getRolle() + "\t\t\t\t");
-            System.out.print(m.getExpertise() + "\t\t\t\t");
-
-            // System.out.println(String.format("%30s %25s %10s %25s %10s %20s %20s",
-            // "Item", "|", "Price($)", "|", "Qty", "|", "ID"));
+            System.out.print(m.getVorname() + TABS);
+            System.out.print(m.getName() + TABS);
+            System.out.print(m.getAbteilung() + TABS);
+            System.out.print(m.getRolle() + TABS);
+            System.out.print(m.getExpertise() + TABS);
         }
-
-    }
-
-    public void tabelle(MitarbeiterContainer a, String exp) {
-
     }
 
     public static void main(String[] args) {
@@ -37,8 +28,7 @@ public class TabellenAusgabe {
             a.addMember(new Mitarbeiter("Lisa", "Franz", "AB", "ABB", x));
             a.addMember(new Mitarbeiter("Lisa", "Franz", "AB", "ABB", x));
         } catch (ContainerException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            System.err.println(e.getMessage());
         }
 
         f.dumpSorted(a.getCurrentListCopy());
