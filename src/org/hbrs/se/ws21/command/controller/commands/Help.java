6bd593ac3e2fd8b0847a03ac139b3031b9ebbcd6
John Meyerhoff <John@r73.de>
2021-11-11 20:39:28 +0100

    improve structure

--- src/org/hbrs/se/ws21/command/controller/commands/Help.java
+++ src/org/hbrs/se/ws21/command/controller/commands/Help.java
@@ -1,20 +1,21 @@
 package org.hbrs.se.ws21.command.controller.commands;
 
-import org.hbrs.se.ws21.command.view.ConsoleUI;
 import java.io.PrintStream;
-public class Help implements Command {
-    PrintStream outstream;
-    public Help(){
+
+import org.hbrs.se.ws21.command.view.ConsoleUI;
+
+public class Help extends ConsoleCommand {
+    public Help() {
         this(System.out);
     }
-    public Help(PrintStream outstream){
+
+    public Help(PrintStream outstream) {
         this.outstream = outstream;
     }
 
-    public void execute(){
-    new ConsoleUI(outstream).displayWelcomeMessage();
-}
+    @Override
+    public void execute() {
+        new ConsoleUI(super.outstream).displayWelcomeMessage();
+    }
 
 }
-
-
