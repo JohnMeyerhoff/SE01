ae5a9207f96f93532fe80f223fe615e11a71c534
John Meyerhoff <John@r73.de>
2021-11-12 16:51:26 +0100

    Update Load.java

--- src/org/hbrs/se/ws21/command/controller/commands/Load.java
+++ src/org/hbrs/se/ws21/command/controller/commands/Load.java
@@ -6,6 +6,7 @@ import org.hbrs.se.ws21.command.view.ConsoleUI;
 
 public class Load extends ConsoleCommand {
     public Load() {
+        // TODO: PARAMETER HINZUFÜGEN
         this(System.out);
     }
 
@@ -15,22 +16,20 @@ public class Load extends ConsoleCommand {
 
     @Override
     public void execute() {
-            // TODO: Streams beheben in commands
-            String parameter = new ConsoleUI(super.outstream).loadDialogue(eingabe);
-            try {
-                if (parameter.equals("merge")) {
-                    speicher.merge();
-                } else {
-                    speicher.force();
-                }
-                // We do not need a boolean because an exception
-                // in merge() would skip this line.
-                new ConsoleUI(super.outstream).displayLoadSucessMessage();
-            } catch (Exception e) {
-                new ConsoleUI(super.outstream).displayLoadFailureMessage(e);
+        // TODO: Streams beheben in commands
+        String parameter = new ConsoleUI(super.outstream).loadDialogue(eingabe);
+        try {
+            if (parameter.equals("merge")) {
+                speicher.merge();
+            } else {
+                speicher.force();
             }
+            // We do not need a boolean because an exception
+            // in merge() would skip this line.
+            new ConsoleUI(super.outstream).displayLoadSucessMessage();
+        } catch (Exception e) {
+            new ConsoleUI(super.outstream).displayLoadFailureMessage(e);
         }
-
-    
+    }
 
 }
