7b1d9bf3fc16780fe8a5b2f4bbf3923bda55b929
John <john@meyerhoff.org>
2022-01-13 17:01:41 +0100

    Add and Implement existing test

---
+++ src/org/hbrs/se/ws21/uebung11/MyPoint.java
@@ -0,0 +1,40 @@
+package org.hbrs.se.ws21.uebung11;
+
+public class MyPoint implements Comparable<MyPoint> {
+
+    private double x;
+    private double y;
+    private static final double EPSILON = 0.0001d;
+
+    public MyPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(MyPoint o) {
+        if ((Math.abs(this.x - o.x) < EPSILON)) {
+            return 0;
+        } else {
+            return (int) (this.y - o.y);
+        }
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof MyPoint other) {
+            return ((Math.abs(this.y - other.y) < EPSILON)
+                    && (Math.abs(this.x - other.x) < EPSILON));
+
+        } else {
+            return false;
+        }
+
+    }
+
+    public MyPoint getHalfwayPoint(MyPoint upperRight) {
+        return new MyPoint((this.x+upperRight.x) /2, (this.y + upperRight.y) / 2);
+    }
+
+}
---
+++ src/org/hbrs/se/ws21/uebung11/MyPrettyRectangle.java
@@ -0,0 +1,30 @@
+package org.hbrs.se.ws21.uebung11;
+
+public class MyPrettyRectangle {
+    private MyPoint lowerLeft;
+    private MyPoint upperRight;
+
+    public MyPrettyRectangle(MyPoint ll, MyPoint ur) {
+        if (ll.compareTo(ur) > 0) {
+            this.lowerLeft = ur;
+            this.upperRight = ll;
+        } else {
+            this.lowerLeft = ll;
+            this.upperRight = ur;
+        }
+    }
+
+    public MyPrettyRectangle(double d, double e, double f, double g) {
+        this(new MyPoint(d, e), new MyPoint(f, g));
+    }
+
+    public boolean contains(MyPrettyRectangle potentialInner) {
+        return (lowerLeft.compareTo(potentialInner.lowerLeft) <= 0)
+                && (upperRight.compareTo(potentialInner.upperRight) >= 0);
+    }
+
+    public MyPoint getCenter() {
+        return lowerLeft.getHalfwayPoint(upperRight);
+    }
+
+}
