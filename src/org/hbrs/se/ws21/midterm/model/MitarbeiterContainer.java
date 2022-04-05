de43df45a554f08d43f927e2038add013a0ba1a6
John <john@meyerhoff.org>
2022-04-05 14:43:33 +0200

    add sprintcontainer into the hierarchy

--- src/org/hbrs/se/ws21/midterm/model/MitarbeiterContainer.java
+++ src/org/hbrs/se/ws21/midterm/model/MitarbeiterContainer.java
@@ -3,13 +3,6 @@ package org.hbrs.se.ws21.midterm.model;
 //Dieses Aufgabenblatt ist in Teamarbeit von Klara Golubovic 
 //und Johannes Meyerhoff bearbeitet worden.
 
-import java.util.ArrayList;
-import java.util.List;
-import org.hbrs.se.ws21.midterm.model.exception.ContainerException;
-import org.hbrs.se.ws21.midterm.model.exception.PersistenceException;
-import org.hbrs.se.ws21.midterm.model.exception.PersistenceException.ExceptionType;
-import org.hbrs.se.ws21.midterm.model.persistence.PersistenceStrategy;
-
 public class MitarbeiterContainer extends Container<Mitarbeiter> {
     private static MitarbeiterContainer instance;
 
