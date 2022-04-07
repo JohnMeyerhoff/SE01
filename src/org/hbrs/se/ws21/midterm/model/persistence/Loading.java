24ef93c86372f5dd9add4d7706d61b512d93555b
John <john@r73.de>
2022-04-07 18:45:52 +0200

    Add loading helper

---
+++ src/org/hbrs/se/ws21/midterm/model/persistence/Loading.java
@@ -0,0 +1,42 @@
+package org.hbrs.se.ws21.midterm.model.persistence;
+
+public class Loading {
+
+    private final String vorname;
+    private final String nachname;
+    private final String rolle;
+    private final String abteilung;
+    private final String expertiseI;
+    private final int expertiseLevelI;
+    private final String expertiseII;
+    private final int expertiseLevelII;
+    private final String expertiseIII;
+    private final int expertiseLevelIII;
+
+    public Loading(String vorname, String nachname, String rolle, String abteilung,
+            String expertiseI, int expertiseLevelI, String expertiseII,
+            int expertiseLevelII, String expertiseIII, int expertiseLevelIII) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.rolle = rolle;
+        this.abteilung = abteilung;
+        this.expertiseI = expertiseI;
+        this.expertiseLevelI = expertiseLevelI;
+        this.expertiseII = expertiseII;
+        this.expertiseLevelII = expertiseLevelII;
+        this.expertiseIII = expertiseIII;
+        this.expertiseLevelIII = expertiseLevelIII;
+    }
+
+    public String toInputString() {
+        return "enter new mitarbeiter " + vorname + " " + nachname + " " + rolle + " "
+                + abteilung + " " + expertiseI + " " + expertiseLevelI + " " + expertiseII
+                + " " + expertiseLevelII + " " + expertiseIII + " " + expertiseLevelIII
+                + " ";
+    }
+
+    public static String saveAndQuit() {
+        return "store exit\n";
+    }
+
+}
